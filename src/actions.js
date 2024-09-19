cf74784fb18f71871dab5c727417b093e0b0063e
vaneck237 <vaneckbilongo@gmail.com>
2024-09-19 10:54:06 +0100

    add covered to graphql

--- src/actions.js
+++ src/actions.js
@@ -40,6 +40,7 @@ const MEDICAL_SERVICE_FULL_PROJECTION = (mm) => [
   "validityTo",
   "level",
   "category",
+  "covered"
 ];
 
 const MEDICAL_ITEM_FULL_PROJECTION = (mm) => [
@@ -57,6 +58,7 @@ const MEDICAL_ITEM_FULL_PROJECTION = (mm) => [
   "validityFrom",
   "validityTo",
   "package",
+  "covered"
 ];
 
 export function formatMedicalItemOrServiceGQL(mm, ms) {
